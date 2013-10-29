3d5a72e3e28083acef5bb3136b4d56a6a51540a9
huybrechtsg <goeric-huybrechts@hotmail.com>
2013-10-29 17:06:50 +0100

    Update Main.java

--- Magazines/src/Main.java
+++ Magazines/src/Main.java
@@ -1,8 +1,13 @@
-// SYNTYCHE
-
 import java.util.HashMap;
 import java.util.Scanner;
 
+/**
+ * 
+ * Classe Main
+ * 
+ * @author Goeric Huybrechts
+ * @version 2013
+ */
 public class Main {
 	
 	public static void main(String[] args) {
